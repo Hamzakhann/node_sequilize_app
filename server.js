0c8d878e160b273be83b86b3b7170e213abceeb2
Hamzakhann <hamzakhann66@gmail.com>
2020-04-02 15:20:06 +0500

    change in node modules

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const connection = new Sequelize("db" , "user" , "pass" ,{
     host:'localhost',
     dialect:"sqlite",
     storage:'db.sqlite',
-    // operatorsAliases:false
+    // operatorsAliases:fals
 })
 
 const User = connection.define("User" , {
